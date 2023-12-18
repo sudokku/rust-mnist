dbb88ffe45ca6d6a274f3316e437db8b03341aad
5udoku <mvys9abg2tfajwzd5r@bk.ru>
2023-12-18 16:08:44 +0200

    Implement some base traits for Matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,7 @@
 use rand::prelude::*;
+use std::ops::{Add, Sub, Mul};
 
+#[derive(Debug)]
 pub struct Matrix{
     pub rows: usize,
     pub cols: usize,
@@ -24,6 +26,72 @@ impl Matrix{
                 res.data[i][j] = rng.gen::<f64>() * 2.0 - 1.0;
             }
             
+        }
+
+        res
+    }
+}
+
+impl Add for Matrix {
+    type Output = Matrix;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.rows != rhs.rows || self.cols != rhs.cols {
+            panic!("Matrices are of incorrect sizes")
+        }
+
+        let mut res = Matrix::zeros(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                res.data[i][j] = self.data[i][j] + rhs.data[i][j];
+            }
+        }
+
+        res
+    }
+}
+
+impl Sub for Matrix {
+    type Output = Matrix;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        if self.rows != rhs.rows || self.cols != rhs.cols {
+            panic!("Matrices are of incorrect sizes")
+        }
+
+        let mut res = Matrix::zeros(self.rows, self.cols);
+
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                res.data[i][j] = self.data[i][j] - rhs.data[i][j];
+            }
+        }
+
+        res
+    }
+}
+
+impl Mul for Matrix {
+    type Output = Matrix;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        if self.cols != rhs.rows {
+            panic!("Matrices are of incorrect sizes")
+        }
+
+        let mut res =  Matrix::zeros(self.rows, rhs.cols);
+        
+        for i in 0..res.rows {
+            for j in 0..res.cols {
+                let mut sum = 0.0;
+                
+                for k in 0..self.cols {
+                    sum += self.data[i][k] * rhs.data[k][j];
+                }
+
+                res.data[i][j] = sum;
+            }
         }
 
         res
